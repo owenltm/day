e00c1a0b92d787600091c0e3461b9e140fc91b28
owenltm <owenlwiantoro01@gmail.com>
2020-07-31 23:30:27 +0800

    add night/day

--- script.js
+++ script.js
@@ -5,6 +5,18 @@ function checkTime(i) {
     return i;
 }
 
+function setLight(){
+    document.getElementById("timetxt").style.color = "#212121";
+    document.getElementById("greettxt").style.color = "#212121";
+    document.body.style.backgroundColor = "#f5f5f5";
+}
+
+function setDark(){
+    document.getElementById("timetxt").style.color = "white";
+    document.getElementById("greettxt").style.color = "white";
+    document.body.style.backgroundColor = "#212121";
+}
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
@@ -15,16 +27,21 @@ function startTime() {
     s = checkTime(s);
 
     document.getElementById('timetxt').innerHTML = h + ":" + m + ":" + s;
+
+    if(h >= 6 && h < 17){
+        setLight();
+    } else {
+        setDark();
+    }
+
     if(h >= 5 && h < 12){
         document.getElementById("greettxt").innerHTML = "Pagiii, semoga harimu menyenangkan :)";
-    } else if (h)
-    if(h >= 12 && h < 17){
+    } else if(h >= 12 && h < 17){
         document.getElementById("greettxt").innerHTML = "Siangg, sudah makan belomm ??";
     } else if (h >=17 && h < 19){
         document.getElementById("greettxt").innerHTML = "Soreee, inget mandi yaaaa wkwkwk";
     } else {
-        document.body.style.backgroundColor = "#1a237e";
-        document.getElementById("greettxt").innerHTML = "Selamat malam, tidur yang nyenyakk ❤️";
+        document.getElementById("greettxt").innerHTML = "Selamat malam, tidur yang nyenyakk";
     }
 
     t = setTimeout(function () {
