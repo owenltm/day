e60024edaf5b22dd21cb2c3333c7c2b38b2b3ac3
owenltm <owenlwiantoro01@gmail.com>
2020-08-01 00:56:10 +0800

    add stars

--- script.js
+++ script.js
@@ -15,6 +15,7 @@ function setDark(){
     document.getElementById("timetxt").style.color = "white";
     document.getElementById("greettxt").style.color = "white";
     document.body.style.backgroundColor = "#212121";
+
 }
 
 function startTime() {
@@ -34,7 +35,7 @@ function startTime() {
         setDark();
     }
 
-    if(h >= 5 && h < 12){
+    if(h > 5 && h < 12){
         document.getElementById("greettxt").innerHTML = "Pagiii, semoga harimu menyenangkan :)";
     } else if(h >= 12 && h < 17){
         document.getElementById("greettxt").innerHTML = "Siangg, sudah makan belomm ??";
@@ -49,3 +50,76 @@ function startTime() {
     }, 500);
 }
 startTime();
+
+const particles = [];
+
+function setup(){
+    createCanvas(window.innerWidth, window.innerHeight);
+
+    const particleAmount = Math.floor(window.innerWidth / 30);
+
+    for(let i = 0; i < particleAmount; i++){
+        particles.push(new Particle());
+    }
+}
+
+function draw(){
+    var today = new Date();
+    var h = today.getHours();
+
+    if(h >= 19 || h < 6){
+        background("#212121");
+        particles.forEach((p, index) => {
+            p.update();
+            p.draw();
+        })
+    }
+}
+
+class Particle {
+    constructor() {
+        //position
+        this.pos = createVector(random(width), random(height));
+        //velocity
+        this.vel = createVector(random(-50, 50), random(-50, 50));
+        //size
+        this.size = 10;
+        //opacity
+        this.opacity = random(1);
+        this.glowing = 0.01;
+    }
+
+    //move by add velocity
+    update() {
+        if(this.opacity < 0.05){
+            this.pos.add(createVector(random(-200, 200), random(-200, 200)));
+        }
+        this.opacity += this.glowing;
+        this.edges();
+        this.glow();
+    }
+
+    draw(){
+        noStroke();
+        fill('rgba(255,255,255,' + this.opacity + ')');
+        circle(this.pos.x, this.pos.y, this.size);
+    }
+
+    edges(){
+        if(this.pos.x < 0 || this.pos.x > width){
+            this.vel.x *= -1;
+        }
+
+        if(this.pos.y < 0 || this.pos.y > height){
+            this.vel.y *= -1;
+        }
+    }
+
+    glow(){
+        if(this.opacity > 0.95){
+            this.glowing *= -1;
+        } else if (this.opacity < 0.05){
+            this.glowing *= -1;
+        }
+    }
+}
