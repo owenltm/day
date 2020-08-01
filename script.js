8c910932d658d77754c68f488f6bf2da471320d1
owenltm <owenlwiantoro01@gmail.com>
2020-08-01 13:24:27 +0800

    add cloud

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function checkTime(i) {
 function setLight(){
     document.getElementById("timetxt").style.color = "#212121";
     document.getElementById("greettxt").style.color = "#212121";
-    document.body.style.backgroundColor = "#f5f5f5";
+    document.body.style.backgroundColor = "#bbdefb";
 }
 
 function setDark(){
@@ -57,10 +57,21 @@ function setup(){
     createCanvas(window.innerWidth, window.innerHeight);
 
     const particleAmount = Math.floor(window.innerWidth / 30);
+    const cloudAmount = Math.floor(window.innerWidth / 150);
 
-    for(let i = 0; i < particleAmount; i++){
-        particles.push(new Particle());
+    var today = new Date();
+    var h = today.getHours();
+
+    if(h >= 19 || h < 6){
+        for(let i = 0; i < particleAmount; i++){
+            particles.push(new Star());
+        }
+    } else {
+        for(let i = 0; i < cloudAmount; i++){
+            particles.push(new Cloud());
+        }
     }
+    
 }
 
 function draw(){
@@ -69,14 +80,44 @@ function draw(){
 
     if(h >= 19 || h < 6){
         background("#212121");
-        particles.forEach((p, index) => {
-            p.update();
-            p.draw();
-        })
+        
+    } else {
+        background("#90caf9");
+    }
+
+    particles.forEach((p, index) => {
+        p.update();
+        p.draw();
+    })
+}
+
+class Cloud {
+    constructor(){
+        this.pos = createVector(random(width), random(height));
+    }
+
+    update(){
+        this.pos.x += random(2);
+
+        if(this.pos.x > width){
+            this.pos.x = 0;
+        }
+    }
+
+    draw(){
+        stroke(255);
+        strokeWeight(1);
+        fill(255);
+        ellipse(this.pos.x, this.pos.y, 56, 56);
+        ellipse(this.pos.x+15,this.pos.y+10, 56, 56);
+        ellipse(this.pos.x+45,this.pos.y+10, 48, 56);
+        ellipse(this.pos.x+40,this.pos.y-10, 56, 48);
+        ellipse(this.pos.x+30,this.pos.y-10, 56, 56);
+        ellipse(this.pos.x+50,this.pos.y, 56, 56);
     }
 }
 
-class Particle {
+class Star {
     constructor() {
         //position
         this.pos = createVector(random(width), random(height));
